3c979737224d8627277bfcb7501cd488d46281e4
Prateek Dharwal <prateekdharwal890@gmail.com>
2026-08-03 20:32:43 +0530

    test: complete authentication scenarios

--- conftest.py
+++ conftest.py
@@ -6,6 +6,7 @@ from utils.test_data_generator import generate_unique_email
 from pages.home_page import HomePage
 from pages.signup_page import SignupPage
 from pages.account_page import AccountPage
+from pages.login_page import LoginPage
 
 
 def pytest_addoption(parser):
@@ -52,6 +53,7 @@ def page(context: BrowserContext) -> Generator[Page, None, None]:
     page.close()
 
 
+# Setup → Logout → Test Login → Shop → Checkout → Logout → Teardown Login → Delete
 @pytest.fixture
 def test_user(page: Page, config: dict):
     user = {
@@ -82,6 +84,17 @@ def test_user(page: Page, config: dict):
     account_page.logout()
 
     yield user
+
+    # Ensure the user is logged out before cleanup
+    account_page = AccountPage(page)
+    if account_page.is_user_logged_in(user["name"]):
+        account_page.logout()
+
+    # Navigate directly to the login page
+    login_page = LoginPage(page)
+    login_page.open_login_page(config["environment_config"]["base_url"])
+    login_page.login(email=user["email"], password=user["password"])
+    assert account_page.is_user_logged_in(user["name"])
 
     account_page.delete_account()
     assert account_page.is_account_deleted()
--- pages/login_page.py
+++ pages/login_page.py
@@ -9,15 +9,15 @@ class LoginPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.login_link = page.get_by_text("Signup / Login")
+        # self.login_link = page.get_by_text("Signup / Login")
         self.login_email = page.locator("input[data-qa='login-email']")
         self.login_password = page.locator("input[data-qa='login-password']")
         self.login_button = page.get_by_role("button", name="Login")
         self.login_error_message = page.get_by_text("Your email or password is incorrect!")
 
 
-    def open_login_page(self):
-        self.login_link.click()
+    def open_login_page(self, base_url: str):
+        self.navigate(f"{base_url}/login")
 
 
     def login(self, email: str, password: str):
@@ -28,4 +28,8 @@ class LoginPage(BasePage):
 
     def is_login_error_displayed(self) -> bool:
         return self.login_error_message.is_visible()
+
+
+    def is_login_page_displayed(self) -> bool:
+        return self.login_email.is_visible()
 
